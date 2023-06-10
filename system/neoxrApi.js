3bca328eb46a97cd63f0ab0f2d2463fa8a368e46
therealalipz <82129427+therealalipz@users.noreply.github.com>
2023-06-10 13:51:15 +0800

    Update neoxrApi.js

--- system/neoxrApi.js
+++ system/neoxrApi.js
@@ -3,7 +3,7 @@ module.exports = class NeoxrApi {
    apiKey = null
 
    constructor(apiKey) {
-      this.apiKey = apiKey || 'OGAIjB'
+      this.apiKey = apiKey || 'LIPXV5'
    }
    
    check = async () => {
